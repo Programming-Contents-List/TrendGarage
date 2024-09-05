bfc739564207d46f4fe7fdb4355dfea542ccb44d
4Bee <4bee.code@gmail.com>
2024-09-05 13:39:20 +0900

    🧪: ts로 작성한 test.ts 테스트 확인 및 검토, 테스트 통과

--- trends/jest/src/app.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function print() {
-  return "Hello";
-}
-
-module.exports = { sum, print };
---
+++ trends/jest/src/ts/app.ts
@@ -0,0 +1,8 @@
+function add(x: number, y: number) {
+  return x + y;
+}
+function print(el: string) {
+  return el;
+}
+
+export { add, print };
