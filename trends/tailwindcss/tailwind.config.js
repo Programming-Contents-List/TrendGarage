16b30c0b944879aef6e6c7de9b56f4b4ab2bcaad
4Bee <4bee.code@gmail.com>
2024-09-14 15:02:37 +0900

    🚩: import 사용에 문제가 되어 tailwild.config에서 전역적으로 스타일링 정의하는 방식으로 커스텀 후 적용 및 확인 성공

--- trends/tailwindcss/tailwind.config.js
+++ trends/tailwindcss/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './TextAdventureGame/src/**/*.{html,js,ts,css,scss}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'padding-01': '18rem',
+        'padding-02': '21rem',
+        'padding-03': '24rem',
+      },
+      colors: {
+        primary: "#000000",
+        sub: "#D9D9D9",
+        status1: "#000000",
+        status2: "#222222",
+        errorCancel: "#FF0000",
+        success: "#0038FF",
+        safety: "#32BF00",
+      }
+    },
   },
   plugins: [],
 }
