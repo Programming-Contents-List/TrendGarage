ec8c25c5b8954108c01f2ba7c7f3a5756f602c4f
4Bee <4bee.code@gmail.com>
2024-11-24 18:48:06 +0900

    🔧: react-router-dom 설치 후 컴포넌트 연결 및 전체적인 테스트 검토 완료

---
+++ front/src/Page/MyTest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyTest = () => {
+  return (
+    <div className='py-10 text-center text-white bg-blue-500'>
+      <h1 className='text-4xl font-bold'>Hello, Tailwind CSS with pnpm!</h1>
+      <span className='bg-slate-500'>test</span>
+    </div>
+  );
+};
+
+export default MyTest;
---
+++ front/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // 사용할 파일들
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
