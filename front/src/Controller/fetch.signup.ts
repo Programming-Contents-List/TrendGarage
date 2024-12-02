9e0517f53f61c545f2b688ce74d09bf125173399
4Bee <109135643+4BFC@users.noreply.github.com>
2024-12-03 00:30:54 +0900

    🔀: 회원가입의 디테일한 raw_user_meta_data를 설정한다. (#28)
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🌱:  README을 수정
    
    Github에서 수정한 커밋입니다.
    
    * 🔧: pnpm으로 CRA template TypeScript를 설치하고 eslint 규칙 적용
    
    * 🔧: react-router-dom 설치 후 컴포넌트 연결 및 전체적인 테스트 검토 완료
    
    * 🔧: pnpm으로 mono-repo 구현
    
    * 🔧: supabase와 axios, tanstackquery 설치
    
    * ✨: Signup 페이지 구현 및 Post요청 확인
    
    * 🔧: dotenv 설치
    
    * ✨: env 파일 ignore에 추가
    
    * ✨: 코드 수정
    
    * ✨: fetch함수 확인 및 post 테스트, 500 에러

---
+++ front/src/Controller/fetch.signup.ts
@@ -0,0 +1,47 @@
+import supabaseInstance from '../Model/fetch.supabase.instance';
+
+interface SignUpParams {
+  email: string;
+  password: string;
+  admin: boolean;
+  job?: string; // Optional: name은 필수가 아닐 수 있음
+  nickname?: string;
+}
+
+const fetchSignUp = async ({ email, password, admin, job, nickname }: SignUpParams) => {
+  try {
+    const { data, error } = await supabaseInstance.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+          // 사용자 정의 메타데이터로 전달
+          admin,
+          job,
+          nickname,
+        },
+      },
+    });
+
+    // 구조할당으로 responseMessage를 보내는 이유는 이 구조로 호출하는 쪽에서 명확하게 responseMessage와 data를 구분해서 사용할 수 있게 하기 위함이다. 즉, 반환 값은 객체이기 때문이다.
+    // 추가적으로 fetchSignup은 promise를 반환했다.
+
+    if (error) {
+      // error
+      console.error('SignUp Error:', error);
+      const errorMessage = error.message || 'Signup failed. Please try again.';
+      return { responseMessage: errorMessage, data: undefined };
+    }
+    // 성공적인 경우
+    const successMessage = 'Signup successful! Check your email for confirmation.';
+    return { responseMessage: successMessage, data };
+
+    // 예외가 발생할 경우
+  } catch (error: any) {
+    console.error('SignUp Error:', error);
+    const catchMessage = error.message || 'An unexpected error occurred. Please try again.';
+    return { responseMessage: catchMessage, data: undefined };
+  }
+};
+
+export default fetchSignUp;
