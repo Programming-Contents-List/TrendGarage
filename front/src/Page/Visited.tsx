38f241b47cb9375645131ba15f8e162e4a881732
4Bee <4bee.code@gmail.com>
2024-12-04 18:15:14 +0900

    ✨: 방명록 페이지 생성

---
+++ front/src/Page/Visited.tsx
@@ -0,0 +1,15 @@
+const Visited = () => {
+  return (
+    <div>
+      <div className='text-3xl'>방명록</div>
+      <div>
+        <textarea placeholder='방명록을 적어주세요~(〜￣▽￣)〜' />
+      </div>
+      <div>
+        <div>전체 댓글</div>
+      </div>
+    </div>
+  );
+};
+
+export default Visited;
