7a2aa5d9d2580acaecd12c4356465cfc42f4fd2a
4Bee <4bee.code@gmail.com>
2024-12-06 01:38:18 +0900

    💄: CSS 적용 완료 및 테스트, 방명록 등록 부분 UI 배치 검토 필요

--- front/src/Page/Visited.tsx
+++ front/src/Page/Visited.tsx
@@ -37,12 +37,13 @@ const Visited = () => {
   }, [isData, isLoading, isError, isSuccess]);
 
   return (
-    <div className='flex items-center justify-center min-h-screen'>
+    <div className='flex items-center justify-center min-h-screen mt-32'>
       {/* max-w-md || mx-auto w-96 */}
-      <div className='flex flex-col p-4 bg-white rounded shadow-md w-96'>
+      <div className='flex flex-col w-6/12'>
+        {/* p-4 bg-white rounded shadow-md */}
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className='text-3xl'>방명록</div>
-          <div className=''>
+          <div className='text-3xl mb-9'>방명록 ヾ(•ω•`)o</div>
+          <div className='flex flex-col mb-5'>
             <textarea
               {
                 // 유효성 검사
@@ -56,24 +57,40 @@ const Visited = () => {
               }
               rows={4}
               placeholder='방명록을 적어주세요~(〜￣▽￣)〜'
-              className='w-full border-2'
+              className='w-full p-2 border-2 rounded-md'
             />
             {/* 에러 메시지 표시 */}
             {/* typeof나 타입 단언(Type Assertion)을 사용하여 errors.message?.message가 string임을 명시적으로 알려주어야 한다. */}
+            <div className='flex justify-end text-gray-300'>
+              <span>0</span>/<span>100</span>
+            </div>
             {errors.message?.message && (
               <p className='mt-1 text-sm text-red-500'>
                 {typeof errors.message.message === 'string' ? errors.message.message : ''}
               </p>
             )}
           </div>
-          <button type='submit' className='w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600'>
-            방명록 작성하기
+          <button
+            type='submit'
+            className='w-full px-4 py-2 mb-4 text-white bg-blue-500 rounded-md hover:bg-blue-600'
+          >
+            등록 (ง •_•)ง
           </button>
         </form>
         <div>
-          <div>전체 댓글</div>
+          {/* <div>전체 댓글</div> */}
           {isData.map((el) => {
-            return <div key={el.id}>{el.message}</div>;
+            return (
+              <div key={el.id} className='p-5 border-2 rounded-md mb-9'>
+                <div className=''>{el.message}</div>
+                <div className='flex justify-end w-full mt-4'>
+                  <div className='flex justify-between w-20 text-gray-500'>
+                    <span className='cursor-pointer hover:text-blue-500'>수정</span>
+                    <span className='cursor-pointer hover:text-blue-500'>삭제</span>
+                  </div>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
